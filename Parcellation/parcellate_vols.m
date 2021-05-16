6d4fc192c3e71d72c6a70a275c14545e2069d699
Dom <dominicd7@hotmail.com>
2021-05-16 15:59:07 +0200

    Change vol parcellation output to match what rDCM expects

--- Parcellation/parcellate_vols.m
+++ Parcellation/parcellate_vols.m
@@ -11,7 +11,7 @@ dataDir = fullfile(datasetDir, 'data', sprintf('sub-%04d', subID));
 glm_dir = fullfile(dataDir, 'GLM_output'); %Specify path to SPM file (GLM output)
 
 atlas = spm_vol(atlasFile);
-data = spm_vol(fullfile(glm_dir,'ResMS.nii'));
+data = spm_vol(fullfile(dataDir,'rsfmri','smooth_norm_fmap_slicecorr_vol.nii'));
 
 atlas_vol = spm_read_vols(atlas);
 data_vols = spm_read_vols(data);
@@ -19,11 +19,11 @@ data_vols = spm_read_vols(data);
 nlabels = nnz(unique(atlas_vol));
 nvols = size(data_vols, 4);
 
-timeseries = zeros(nlabels,nvols);
+timeseries = zeros(nvols,nlabels);
 
 for label = 1:nlabels
 	% take the mean of the volume corresponding to the label, for each volume in the timeseries
-	timeseries(label,:) = nanmean(reshape(data_vols(repmat(atlas_vol == label, 1,1,1,nvols)), [], nvols), 1);
+	timeseries(:,label) = nanmean(reshape(data_vols(repmat(atlas_vol == label, 1,1,1,nvols)), [], nvols), 1);
 end
 
 save(fullfile(glm_dir, 'extraced_timeseries.mat'), 'timeseries');
--- rDCM/rdcm.m
+++ rDCM/rdcm.m
@@ -4,11 +4,11 @@
 % Y should be in correct format straight from SPM pre-processing
 % U is "switched off" by setting all input parameters to zero (second parameter)
 % No args (third parameter)
-DCM = tapas_rdcm_model_specification(Y, [], []) 
+DCM = tapas_rdcm_model_specification(struct('y',timeseries,'dt',2.5), [], []);
 
 % specify the options for the rDCM analysis - from TAPAS rDCM tutorial
 options.SNR             = 3; % signal-to-noise ratio of synthetic fMRI data (for simulations)
-options.y_dt            = 0.5; % repetition time (TR)
+options.y_dt            = 2.5; % repetition time (TR)
 options.p0_all          = 0.15;  % single p0 value (for computational efficiency) - only relevant for rDCM with sparsity constraints
 options.iter            = 100; % number of permutations (of regressors) per region - only relevant for rDCM with sparsity constraints
 options.filter_str      = 5; % ?
