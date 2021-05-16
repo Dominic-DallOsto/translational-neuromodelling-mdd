114d5d2d64d567a53b62348ec5e3605de64cdd2f
Dom <dominicd7@hotmail.com>
2021-05-16 15:30:15 +0200

    Created script to parcellate .nii file using atlas

---
+++ Parcellation/parcellate_vols.m
@@ -0,0 +1,29 @@
+function parcellate_vols(datasetDir, subID, atlasFile)
+% function extracts the timeseries from multiple ROIs defined in a label
+% atlas 
+%
+% Inputs
+%   datasetDir: path to folder of subjects
+%   subID:      int 
+%   atlasFile:  File containing all ROI with different labels (.nii)
+
+dataDir = fullfile(datasetDir, 'data', sprintf('sub-%04d', subID)); 
+glm_dir = fullfile(dataDir, 'GLM_output'); %Specify path to SPM file (GLM output)
+
+atlas = spm_vol(atlasFile);
+data = spm_vol(fullfile(glm_dir,'ResMS.nii'));
+
+atlas_vol = spm_read_vols(atlas);
+data_vols = spm_read_vols(data);
+
+nlabels = nnz(unique(atlas_vol));
+nvols = size(data_vols, 4);
+
+timeseries = zeros(nlabels,nvols);
+
+for label = 1:nlabels
+	% take the mean of the volume corresponding to the label, for each volume in the timeseries
+	timeseries(label,:) = nanmean(reshape(data_vols(repmat(atlas_vol == label, 1,1,1,nvols)), [], nvols), 1);
+end
+
+save(fullfile(glm_dir, 'extraced_timeseries.mat'), 'timeseries');
