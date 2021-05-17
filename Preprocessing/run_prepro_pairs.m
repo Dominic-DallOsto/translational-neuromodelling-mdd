ddfb834a906a36b229d6ceff1cc00b82b42218a9
Dom <dominicd7@hotmail.com>
2021-05-17 14:22:45 +0200

    Updated preprocessing script

--- Preprocessing/run_prepro_pairs.m
+++ Preprocessing/run_prepro_pairs.m
@@ -1,3 +1,37 @@
 function run_prepro_pairs(dataset_dir, index)
 	pairs = read_pairs_text_file('../Dataset Analysis/COI_perfectpair_pairs.txt');
-	prepro_subject(dataset_dir, pairs(index), 1);
+	
+	subject = pairs(index);
+	subject_dir = fullfile(dataset_dir,'data',sprintf('sub-%04d',subject));
+	
+	% Preprocessing
+	if ~exist(fullfile(subject_dir, 'rsfmri', 'smooth_norm_meanfmap_slicecorr_vol.nii'), 'file')
+		fprintf('Running preprocessing for subject %d.', subject);
+		prepro_subject(dataset_dir, subject, 1);
+	end
+	
+	% Physio
+	if ~exist(fullfile(subject_dir, 'physio_output'), 'dir')
+		fprintf('Running physio for subject %d.', subject);
+		create_physio_regressors(dataset_dir, subject);
+	end
+	
+	% GLM
+	if ~exist(fullfile(subject_dir, 'GLM_output'), 'dir')
+		fprintf('Running GLM for subject %d.', subject);
+		run_firstLevelModel_with_Res(dataset_dir, subject);
+	end
+	
+	% Parcellation
+	if ~exist(fullfile(subject_dir, 'GLM_output', 'extracted_timeseries.mat'), 'file')
+		fprintf('Running parcellation for subject %d.', subject);
+		cd ../Parcellation/
+		parcellate_vols(dataset_dir, subject, 'combined_atlas.nii');
+	end
+	
+	% rDCM (with bandpass filtering)
+	if ~exist(fullfile(subject_dir, 'rDCM'), 'dir')
+		fprintf('Running rDCM for subject %d.', subject);
+		cd ../rDCM
+		run_rDCM(dataset_dir, subject);
+	end
