3a1f87822ff24f3057a50f0c80a0893177646e40
Dom <dominicd7@hotmail.com>
2021-05-22 14:36:21 +0200

    Run just specified steps of preprocessing

--- Preprocessing/run_prepro_pairs.m
+++ Preprocessing/run_prepro_pairs.m
@@ -1,5 +1,9 @@
-function run_prepro_pairs(dataset_dir, index)
+function run_prepro_pairs(dataset_dir, index, steps_to_run)
 	pairs = read_pairs_text_file('../Dataset Analysis/COI_perfect_pairs.txt');
+	
+	if nargin < 3
+		steps_to_run = [];
+	end
 	
 	subject = pairs(index);
 	subject_dir = fullfile(dataset_dir,'data',sprintf('sub-%04d',subject));
@@ -7,39 +11,39 @@ function run_prepro_pairs(dataset_dir, index)
 	scan_properties = get_protocol_data(dataset_dir, patient_data.protocol);
 	
 	% Preprocessing
-	if ~exist(fullfile(subject_dir, 'rsfmri', 'smooth_norm_meanfmap_slicecorr_vol.nii'), 'file')
+	if any(find(steps_to_run == 1)) || ~exist(fullfile(subject_dir, 'rsfmri', 'smooth_norm_meanfmap_slicecorr_vol.nii'), 'file')
 		fprintf('Running preprocessing for subject %d.', subject);
 		prepro_subject(dataset_dir, subject, 1);
 	end
 	
 	% Physio
-	if ~exist(fullfile(subject_dir, 'physio_output'), 'dir')
+	if any(find(steps_to_run == 2)) ||~exist(fullfile(subject_dir, 'physio_output'), 'dir')
 		fprintf('Running physio for subject %d.', subject);
 		create_physio_regressors(dataset_dir, subject);
 	end
 	
 	% GLM
-	if ~exist(fullfile(subject_dir, 'GLM_output'), 'dir')
+	if any(find(steps_to_run == 3)) ||~exist(fullfile(subject_dir, 'GLM_output'), 'dir')
 		fprintf('Running GLM for subject %d.', subject);
 		run_firstLevelModel_with_Res(dataset_dir, subject);
 	end
 	
 	% Parcellation
-	if ~exist(fullfile(subject_dir, 'GLM_output', 'extracted_timeseries.mat'), 'file')
+	if any(find(steps_to_run == 4)) ||~exist(fullfile(subject_dir, 'GLM_output', 'extracted_timeseries.mat'), 'file')
 		fprintf('Running parcellation for subject %d.', subject);
 		cd ../Parcellation/
 		parcellate_vols(dataset_dir, subject, 'combined_atlas.nii');
 	end
 	
 	% rDCM (with bandpass filtering)
-	if ~exist(fullfile(subject_dir, 'rDCM'), 'dir')
+	if any(find(steps_to_run == 5)) ||~exist(fullfile(subject_dir, 'rDCM'), 'dir')
 		fprintf('Running rDCM for subject %d.', subject);
 		cd ../rDCM
 		run_rDCM(dataset_dir, subject, str2double(scan_properties.TR_s_));
 	end
 	
 	% just save the A matrix to reduce space
-	if ~exist(fullfile(subject_dir, 'rDCM', 'dcm_A.mat'), 'file')
+	if any(find(steps_to_run == 5)) || ~exist(fullfile(subject_dir, 'rDCM', 'dcm_A.mat'), 'file')
 		data = load(fullfile(subject_dir, 'rDCM', 'dcm_output.mat'));
 		A = data.output.Ep.A;
 		save(fullfile(subject_dir, 'rDCM', 'dcm_A.mat'), 'A');
