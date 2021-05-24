5f5eb56bd9876acf272e22bcc78e2c4ff413f97f
Dom <dominicd7@hotmail.com>
2021-05-24 13:57:43 +0200

    preprocessing takes subject ids file as input

--- Preprocessing/run_prepro.m
+++ Preprocessing/run_prepro.m
@@ -1,5 +1,5 @@
-function run_prepro(dataset_dir, index, steps_to_run)
-	subjects = read_array_text_file('../Dataset Analysis/COI_all.txt');
+function run_prepro(dataset_dir, subjects_file, index, steps_to_run)
+	subjects = read_array_text_file(subjects_file);
 	
 	if nargin < 3
 		steps_to_run = [];
